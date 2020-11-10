742d4ec6001678a511eaee7d6267645b9624bc87
GayathriRaja <gayathrraja@gmail.com>
2020-11-10 11:41:22 +0530

    University-Project

---
+++ assets/js/campage.js
@@ -0,0 +1,8 @@
+function testJS(){
+    var source = document.getElementById('example-2').value;
+    document.getElementById('get').innerHTML = source;
+}
+//$("#example-2").click(function(){
+  //  $("#get").load( "cam_page.html?id=" + $(this).attr("href").slice(1) );
+    //return false;
+//});
